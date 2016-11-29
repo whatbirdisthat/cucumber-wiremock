86522a5c879a980dea914e297237857616211b48
wbit <whatbirdisthat@gmail.com>
2016-11-29 18:32:18 +1100

    initial commit

---
+++ src/main/groovy/tqxr/containers/CucumberWiremockApplication.groovy
@@ -0,0 +1,12 @@
+package tqxr.containers
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class CucumberWiremockApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run CucumberWiremockApplication, args
+	}
+}
---
+++ src/test/groovy/tqxr/containers/CucumberWiremockApplicationTests.groovy
@@ -0,0 +1,16 @@
+package tqxr.containers
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class CucumberWiremockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
